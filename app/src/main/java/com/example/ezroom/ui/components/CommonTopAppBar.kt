b29a61c0fbe08ee9084c2babc26725f58af7750e
Tran Phong <tranvuphong05@gmail.com>
2026-06-04 12:54:20 +0700

    fix Tai's screen

--- app/src/main/java/com/example/ezroom/ui/components/CommonTopAppBar.kt
+++ app/src/main/java/com/example/ezroom/ui/components/CommonTopAppBar.kt
@@ -1,8 +1,14 @@
 package com.example.ezroom.ui.components
 
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun CommonTopAppBar() {
-    // TODO: Implement Common Top App Bar
+fun CommonTopAppBar(title: String) {
+    TopAppBar(
+        title = { Text(text = title) }
+    )
 }
