4d41fab4a1fda348a004aee20ae5b29a7602ed7a
Tran Phong <tranvuphong05@gmail.com>
2026-09-03 22:32:27 +0700

    fix(contract): fix infinite loading on contract draft preview and update device base url

--- app/src/main/java/com/example/ezroom/util/ApiConfig.kt
+++ app/src/main/java/com/example/ezroom/util/ApiConfig.kt
@@ -4,7 +4,7 @@ import android.os.Build
 
 object ApiConfig {
     // Base URL configuration for emulator and physical device
-    private const val BASE_URL_DEVICE = "http://192.168.2.12:3000/"
+    private const val BASE_URL_DEVICE = "http://192.168.1.85:3000/"
     private const val BASE_URL_EMULATOR = "http://10.0.2.2:3000/"
 
     // Check if running on emulator
